968777fe6e8beda296bc1379646399294830b491
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-07-05 11:13:30 -0700

    v1

--- 3d_heat_map.py
+++ 3d_heat_map.py
@@ -10,6 +10,7 @@ import os
 import sys
 from utilities import *
 import matplotlib.cm
+import json
 
 
 # --------------------------------------------------
@@ -56,8 +57,10 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
+    result_json = {}
     basename = os.path.splitext(os.path.basename(args.downsampled_pointcloud))[0]
     create_out_path(args.out_dir)
+
     # Load previously generated downsampled pointcloud.
     downsampled_pcd = load_pcd(args.downsampled_pointcloud)
 
@@ -67,8 +70,13 @@ def main():
 
     # Generate heatmap from the downsampled pointcloud.
     pcd_image_main,pcd_bounds,pcd_bounds_utm = generate_height_image_from_pcd(downsampled_pcd,main_ortho,ortho_coords)
-
     cv2.imwrite(''.join([os.path.join(args.out_dir, basename), '_heatmap.png']), pcd_image_main)
+
+    result_json = {'pcd_bounds': pcd_bounds,
+                   'pcd_bounds_utm': pcd_bounds_utm}
+
+    with open(''.join([os.path.join(args.out_dir, basename), '.json']), 'w') as outfile:
+        json.dump(result_json, outfile)
 
 
 # --------------------------------------------------
